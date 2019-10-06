b131c3c5b2ad8bea9f57d3b998ce284ee7d727e6
pevzner47 <pevzner47@gmail.com>
2019-10-06 12:57:51 +0300

    поправил, проверил

--- accessor.rb
+++ accessor.rb
@@ -7,7 +7,7 @@ module Accessor
       define_method("#{name}_history".to_sym) { instance_variable_get(history_arr_name) }
       define_method("#{name}=".to_sym) do |value| 
         instance_variable_set(var_name, value)
-        instance_variable_set(history_arr_name, []) if instance_variable_get(history_arr_name)
+        instance_variable_set(history_arr_name, []) if !instance_variable_get(history_arr_name)
         instance_variable_set(history_arr_name, instance_variable_get(history_arr_name) << value)
       end
     end
@@ -29,4 +29,5 @@ class Test
   extend Accessor
 
   strong_attr_accessor :my_attr, :a, :b, Integer
+  attr_accessor_with_history :my_attr, :a, :b
 end
--- validation.rb
+++ validation.rb
@@ -11,33 +11,39 @@ module Validation
       @options_arr ||= []
     end
 
-    def options_arr=(value)
-      @options_arr = value
-    end
-
     def validate(name, type, option = nil)
       options_arr << [name, type, option]
+    end
+
+    private
+    
+    def options_arr=(value)
+      @options_arr = value
     end
   end
 
   module InstanceMethods
 
-    protected
 
     def validate!
       self.class.options_arr.each do |args|
         var = instance_variable_get("@#{args[0]}".to_sym)
         type = args[1]
         option = args[2]
-        case type
-        when :presence
-          return raise "Значение не может быть пустым или nil" if var.nil? or var == ''
-        when :format
-          return raise "Неверный формат" if var.to_s !~ option
-        when :type
-          return raise "Неверный тип аргумента" if !var.is_a? option
-        end
+        send("validate_#{type}", var, option)
       end
+    end
+    
+    def validate_presence(var, option)
+      raise "Значение не может быть пустым или nil" if var.nil? or var == ''
+    end
+
+    def validate_format(var, option)
+      raise "Неверный формат" if var.to_s !~ option
+    end
+
+    def validate_type(var, option)
+      raise "Неверный тип аргумента" if !var.is_a? option
     end
 
     def valid?
